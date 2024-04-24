3b42103219416f5e791ef65451ce6ebe709e8af2
Fernando Hernández Salazar <sistemas13@red5g.co>
2024-04-23 21:49:25 -0500

    cors added

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import requests
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 @app.route('/send', methods=['POST'])
 def send_data():
